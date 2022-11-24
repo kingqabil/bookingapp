497d68b9c7a62bbe10bdcaa2561f9f8ba9a5489d
SadiQ HabiL <kingqabil@live.com>
2022-11-25 00:17:12 +0300

    Add Authentication, password encryption and CRUD

--- mern-booking/api/routes/users.js
+++ mern-booking/api/routes/users.js
@@ -1,6 +1,29 @@
-import express  from "express";
+import express from "express";
+import {
+  updateUser,
+  deleteUser,
+  getUser,
+  getUsers,
+} from "../controllers/user.js";
+import { verifyAdmin, verifyToken, verifyUser } from "../utils/verifyToken.js";
+
 
 const router = express.Router();
 
+router.get("/checkauthentication", verifyToken, (req,res,next)=>{
+  res.send("hello user, you are logged in")
+})
 
-export default router
+//UPDATE
+router.put("/:id", verifyUser, updateUser);
+
+//DELETE
+router.delete("/:id", verifyUser, deleteUser);
+
+//GET
+router.get("/:id", verifyUser, getUser);
+
+//GET ALL
+router.get("/", verifyAdmin, getUsers);
+
+export default router;
