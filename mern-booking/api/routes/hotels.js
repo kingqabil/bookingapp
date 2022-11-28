ba090499417051cd3ab0476c6417630a82d8c73a
SadiQ HabiL <kingqabil@live.com>
2022-11-28 18:29:18 +0300

    Finish home page

--- mern-booking/api/routes/hotels.js
+++ mern-booking/api/routes/hotels.js
@@ -1,55 +1,33 @@
-import express  from "express";
+import express from "express";
+import {
+  countByCity,
+  countByType,
+  createHotel,
+  deleteHotel,
+  getHotel,
+  getHotelRooms,
+  getHotels,
+  updateHotel,
+} from "../controllers/hotel.js";
 import Hotel from "../models/Hotel.js";
-
+import {verifyAdmin} from "../utils/verifyToken.js"
 const router = express.Router();
 
 //CREATE
-router.post("/", async (req,res)=>{
+router.post("/", verifyAdmin, createHotel);
 
-  const newHotel = new Hotel(req.body)
-
-  try{
-    const savedHotel = await newHotel.save();
-    res.status(200).json(savedHotel);
-  }catch(err){
-    res.status(500).json(err);
-  }
-});
 //UPDATE
-router.put("/:id", async (req, res)=>{
-  try{
-    const updatedHotel = await Hotel.findByIdAndUpdate(req.params.id, { $set: req.body }, { new: true });
-    res.status(200).json(updatedHotel);
-  }catch(err){
-    res.status(500).json(err);
-  }
-});
+router.put("/:id", verifyAdmin, updateHotel);
 //DELETE
-router.delete("/:id", async (req, res)=>{
-  try{
-    await Hotel.findByIdAndDelete(req.params.id,);
-    res.status(200).json("Hotel has been deleted.");
-  }catch(err){
-    res.status(500).json(err);
-  }
-});
+router.delete("/:id", verifyAdmin, deleteHotel);
 //GET
-router.get("/:id", async (req, res)=>{
-  try{
-    const hotel = await Hotel.findById(req.params.id,);
-    res.status(200).json(hotel);
-  }catch(err){
-    res.status(500).json(err);
-  }
-});
-//GET ALL
-router.get("/", async (req, res)=>{
-  try{
-    const hotels = await Hotel.find();
-    res.status(200).json(hotels);
-  }catch(err){
-    res.status(500).json(err);
-  }
-});
 
-export default router
+router.get("/find/:id", getHotel);
+//GET ALL
+
+router.get("/", getHotels);
+router.get("/countByCity", countByCity);
+router.get("/countByType", countByType);
+router.get("/room/:id", getHotelRooms);
+
+export default router;
